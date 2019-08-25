d76986a47a60802bb4055af4d376f6a3f101cdf0
MINGYU KIM <momguus@naver.com>
2019-08-25 14:49:21 +0900

    //25일 그룹화면 수정_김민규

--- app/src/main/java/com/example/seoul/Group/GrouplistFragment.java
+++ app/src/main/java/com/example/seoul/Group/GrouplistFragment.java
@@ -7,25 +7,43 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.seoul.CrewcreateActivity;
 import com.example.seoul.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GrouplistFragment extends Fragment {
+
+    private RecyclerView rv;
+    private LinearLayoutManager llm;
+    private List<Integer> count;
+    private int i = 0;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
         View view = inflater.inflate(R.layout.fragment_grouplist, container,false);
+        rv = (RecyclerView) view.findViewById(R.id.recyclerView);
+        llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        count = new ArrayList<>();
+
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(llm);
+
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -36,6 +54,20 @@ public class GrouplistFragment extends Fragment {
         });
         return view;
     }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_btn_add : {
+                i ++;
+                count.add(i);
+                RvAdapter adapter = new RvAdapter(getActivity(), count, i);
+                rv.setAdapter(adapter);
+                Log.d("Count", count + "");
+                adapter.notifyDataSetChanged();
+                break;
+            }
+        }
+    }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
