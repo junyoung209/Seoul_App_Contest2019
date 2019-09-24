1f493005d5e498aea15c04aa36e5e9fb74d67722
junyoung209 <junyoung209@naver.com>
2019-09-24 10:32:04 +0900

    "php 수정!"

---
+++ app/src/main/java/com/example/seoul/Group/Grouplist/GroupAllcrewlistRequest.java
@@ -0,0 +1,17 @@
+package com.example.seoul.Group.Grouplist;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+
+public class GroupAllcrewlistRequest extends StringRequest {
+
+    final static private String URL="http://39.115.93.52:8111/group/grouplist/groupalllistsend.php";
+
+
+    public GroupAllcrewlistRequest(Response.Listener<String> listener){
+        super(Method.POST,URL,listener,null);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/seoul/Group/Grouplist/GroupSurcrewlistRequest.java
@@ -0,0 +1,28 @@
+package com.example.seoul.Group.Grouplist;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GroupSurcrewlistRequest extends StringRequest {
+
+    final static private String URL="http://39.115.93.52:8111/group/grouplist/groupsurlistsend.php";
+    private Map<String,String> parameters;
+
+    public GroupSurcrewlistRequest(String userID, String userRegion , Response.Listener<String> listener){
+        super(Method.POST,URL,listener,null);
+        parameters = new HashMap<>();
+        parameters.put("userID",userID);
+        parameters.put("userRegion",userRegion);
+
+
+    }
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return parameters;
+    }
+
+}
